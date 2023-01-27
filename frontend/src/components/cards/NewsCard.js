695b0dd4286a821c4ee73c74e65744e5208b350e
Александр Фокин <wegnagun@bk.ru>
2023-01-27 13:47:16 +0300

    some changes

--- frontend/src/components/cards/NewsCard.js
+++ frontend/src/components/cards/NewsCard.js
@@ -13,8 +13,8 @@ function NewsCard({ pub_date, title, image, text }) {
         alt={image} /* вставить дефолтное изображение! */
       ></img>
       <h1>{title}</h1>
-      <h3>{pub_date}</h3>
       <h2>{text}</h2>
+      <h3 className="pub-date">дата публикации: {pub_date}</h3>
     </div>
   );
 }
