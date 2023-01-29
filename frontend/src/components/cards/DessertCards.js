031053c53141cb5e9d4cf64634c05b3295df34f6
Александр Фокин <wegnagun@bk.ru>
2023-01-29 16:17:48 +0300

    add fetch to desserts

--- frontend/src/components/cards/DessertCards.js
+++ frontend/src/components/cards/DessertCards.js
@@ -1,15 +1,41 @@
+import { useState, useEffect } from 'react';
 import desserts from '../data/desserts';
 import DessertCard from './DessertCard';
 import './DessertCards.css';
 
 function DessertCards() {
+  const [desserts, setDesserts] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('localhost:8000/api/desserts')
+      .then((response) => response.json())
+      .then((json) => setDesserts(json))
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
+  });
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
   return (
     <div className="dessert-cards">
-      {desserts.map((person) => {
-        return <DessertCard {...person} key={person.id} />;
-      })}
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        desserts.map((dessert) => <DessertCard {...dessert} key={dessert.id} />)
+      )}
     </div>
   );
+
+  /*   return (
+    <div className="dessert-cards">
+      {desserts.map((dessert) => {
+        return <DessertCard {...dessert} key={dessert.id} />;
+      })}
+    </div>
+  ); */
 }
 
 export default DessertCards;
