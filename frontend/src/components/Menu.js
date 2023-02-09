ec66193faa4d7ee81901cff95043fbdabfb40f57
Александр Фокин <wegnagun@bk.ru>
2023-02-09 15:17:40 +0300

    working at burger-menu

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,9 +1,14 @@
+import { Turn as Hamburger } from 'hamburger-react';
+import { useState } from 'react';
+import cn from 'classnames';
 import { NavLink } from 'react-router-dom';
 import styles from './Menu.module.css';
 import Button from './UI/Button';
-import Burger from './Burger';
 
 const Menu = () => {
+  const [menuToggled, setMenuToggled] = useState(false);
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <>
       <nav className={styles.menu}>
@@ -20,9 +25,36 @@ const Menu = () => {
           <Button>Отзывы</Button>
         </NavLink>
       </nav>
-      <nav className={styles.mobilemenu}>
-        <Burger />
-      </nav>
+      <div className={styles.mobilemenu}>
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          direction="left"
+          color="#140114"
+          label="Открыть меню"
+          onToggle={(toggled) => {
+            if (toggled) {
+              setOpen(true);
+            } else {
+              setOpen(false);
+            }
+          }}
+        />
+        <div className={isOpen ? styles.openmenu : styles.closedmenu}>
+          <NavLink to="." className={styles.button}>
+            <Button>Обо мне</Button>
+          </NavLink>
+          <NavLink to="news" className={styles.button}>
+            <Button>Новости</Button>
+          </NavLink>
+          <NavLink to="desserts" className={styles.button}>
+            <Button>Тортики</Button>
+          </NavLink>
+          <NavLink to="reviews" className={styles.button}>
+            <Button>Отзывы</Button>
+          </NavLink>
+        </div>
+      </div>
     </>
   );
 };
