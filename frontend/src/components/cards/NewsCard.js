f3e5796f175f0a60ea3c3f03b892d1bbade5fe11
Александр Фокин <wegnagun@bk.ru>
2023-02-19 13:36:31 +0300

    add default image if no other

--- frontend/src/components/cards/NewsCard.js
+++ frontend/src/components/cards/NewsCard.js
@@ -6,8 +6,7 @@ function NewsCard({ pub_date, title, image, text }) {
     <div className="news-card">
       <img
         className="img"
-        /* src={image !== undefined && image !== null ? image : pic} */
-        src={image}
+        src={image !== null ? image : pic}
         alt="изображение отсутствует("
       ></img>
       <p className="news-title">{title}</p>
