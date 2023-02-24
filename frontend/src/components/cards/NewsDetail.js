a8487316d107f8c9e8d9482bde8814490881fcae
Александр Фокин <wegnagun@bk.ru>
2023-02-24 16:00:47 +0300

    some changes

--- frontend/src/components/cards/NewsDetail.js
+++ frontend/src/components/cards/NewsDetail.js
@@ -1,6 +1,7 @@
 import { ColorRing } from 'react-loader-spinner';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import pic from '../data/no_image.png';
 import './NewsDetail.css';
 
 function NewsDetail() {
