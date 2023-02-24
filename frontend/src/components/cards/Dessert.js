2972cb4d484cbcd45748e31626baead5c97e12ab
Александр Фокин <wegnagun@bk.ru>
2023-02-24 18:29:22 +0300

    delete ColorRing from NewsDetail and Dessert cards

--- frontend/src/components/cards/Dessert.js
+++ frontend/src/components/cards/Dessert.js
@@ -1,7 +1,5 @@
-import { ColorRing } from 'react-loader-spinner';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import desserts_data from '../data/desserts';
 import Ingredients from './Ingredients';
 import pic from '../data/no_image.png';
 import './Dessert.css';
@@ -10,17 +8,7 @@ function Dessert() {
   const { id } = useParams();
   const [dessert, setDessert] = useState([]);
   const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
-  // для тестов!
-  /* const result = desserts_data.filter(function (element) {
-    if (element.id == id) {
-      return element;
-    } else {
-      return false;
-    }
-  });
-  const data = { ...result[0] }; */
-  // для тестов!
+
   useEffect(() => {
     fetch(`/api/desserts/${id}/`, { method: 'GET' })
       .then((response) => response.json())
@@ -33,33 +21,19 @@ function Dessert() {
   }
   return (
     <div className="dessert-item">
-      {isLoading ? (
-        <ColorRing
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="blocks-loading"
-          wrapperStyle={{}}
-          wrapperClass="blocks-wrapper"
-          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-        />
-      ) : (
-        <>
-          <img
-            className="img"
-            src={dessert.image !== undefined ? dessert.image : pic}
-            alt="изображение отсутствует("
-          />
-          <p className="dessert-title">{dessert.name}</p>
-          <p className="dessert-description">{dessert.text}</p>
-          <div className="ingredients-container">
-            <div className="ingredients">
-              {'Состав:\n'}
-              <Ingredients {...dessert.ingredients} />
-            </div>
-          </div>
-        </>
-      )}
+      <img
+        className="img"
+        src={dessert.image !== undefined ? dessert.image : pic}
+        alt="изображение отсутствует("
+      />
+      <p className="dessert-title">{dessert.name}</p>
+      <p className="dessert-description">{dessert.text}</p>
+      <div className="ingredients-container">
+        <div className="ingredients">
+          {'Состав:\n'}
+          <Ingredients {...dessert.ingredients} />
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/cards/NewsDetail.js
+++ frontend/src/components/cards/NewsDetail.js
@@ -1,4 +1,3 @@
-import { ColorRing } from 'react-loader-spinner';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import pic from '../data/no_image.png';
@@ -8,44 +7,30 @@ function NewsDetail() {
   const { id } = useParams();
   const [news, setNews] = useState([]);
   const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch(`/api/news/${id}/`, { method: 'GET' })
       .then((response) => response.json())
       .then((json) => setNews(json))
-      .catch((error) => setError(error.message))
-      .finally(() => setIsLoading(false));
+      .catch((error) => setError(error.message));
   }, []);
   if (error) {
     return <h1>Error: {error}</h1>;
   }
   return (
     <>
-      {isLoading ? (
-        <ColorRing
-          visible={true}
-          height="80"
-          width="80"
-          ariaLabel="blocks-loading"
-          wrapperStyle={{}}
-          wrapperClass="blocks-wrapper"
-          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      <div className="news-item">
+        <img
+          className="img"
+          src={news.image !== undefined ? news.image : pic}
+          alt="изображение отсутствует("
         />
-      ) : (
-        <div className="news-item">
-          <img
-            className="img"
-            src={news.image !== undefined ? news.image : pic}
-            alt="изображение отсутствует("
-          />
-          <p className="news-title">{news.name}</p>
-          <p className="news-text">{news.text}</p>
-          <p className="pub-date">
-            дата публикации: {`${news.pub_date.slice(0, 10)}`}
-          </p>
-        </div>
-      )}
+        <p className="news-title">{news.name}</p>
+        <p className="news-text">{news.text}</p>
+        <p className="pub-date">
+          дата публикации: {`${news.pub_date.slice(0, 10)}`}
+        </p>
+      </div>
     </>
   );
 }
